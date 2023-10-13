c8d150b6272c2f90ee986960c7c7821459340174
Yuriy Starostin <yurik_05@inbox.ru>
2023-10-13 10:33:33 +0400

    feat: fix error

--- src/HexToRgbConverter.jsx
+++ src/HexToRgbConverter.jsx
@@ -2,28 +2,28 @@ import React, { useState } from 'react';
 
 function HexToRgbConverter() {
     const [hexColor, setHexColor] = useState('');
-    const [rgbColor, setRgbColor] = useState('');
-    const [error, setError] = useState('');
+    const [rgbColor, setRgbColor] = useState(null);
 
     const handleHexChange = (e) => {
         const inputHex = e.target.value;
 
         setHexColor(inputHex);
 
-        if (inputHex.length === 7) {
-            if (inputHex.match(/^#[0-9A-Fa-f]{6}$/)) {
-                setError('');
-                const r = parseInt(inputHex.slice(1, 3), 16);
-                const g = parseInt(inputHex.slice(3, 5), 16);
-                const b = parseInt(inputHex.slice(5, 7), 16);
-                setRgbColor(`RGB(${r}, ${g}, ${b})`);
-                document.body.style.backgroundColor = inputHex;
+        if (inputHex.match(/^#[0-9A-Fa-f]*$/)) {
+            if (inputHex.length >= 7 && inputHex.length <= 9) {
+                if (inputHex.length === 7) {
+                    const r = parseInt(inputHex.slice(1, 3), 16);
+                    const g = parseInt(inputHex.slice(3, 5), 16);
+                    const b = parseInt(inputHex.slice(5, 7), 16);
+                    setRgbColor(`RGB(${r}, ${g}, ${b})`);
+                    document.body.style.backgroundColor = inputHex;
+                } else {
+                    setRgbColor('');
+                }
             } else {
-                setError('Ошибка!');
                 setRgbColor('');
             }
         } else {
-            setError('');
             setRgbColor('');
         }
     }
@@ -35,8 +35,8 @@ function HexToRgbConverter() {
                 value={hexColor}
                 onChange={handleHexChange}
             />
-            {error && <p style={{ color: 'red' }}>{error}</p>}
-            {rgbColor && <p>{rgbColor}</p>}
+            {rgbColor !== null ? <p>{rgbColor}</p> : null}
+            {rgbColor === '' && <p style={{ color: 'red' }}>Ошибка!</p>}
         </div>
     );
 }
